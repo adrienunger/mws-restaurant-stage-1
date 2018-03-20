66290c6d37939aa462c29d513b1826626ad76125
Adrien <email@email.com>
2018-03-20 20:33:33 +0100

    feat: Added off-canvas pattern for filter-options on small screens
    To make small screens less crowded the filter options will be positioned off canvas on small screens and can be brought into view by clicking a menu button.
    Many small improvements to responsible page design.

---
+++ js/off_canvas.js
@@ -0,0 +1,14 @@
+      /*
+       * Open the filter-options menu when the menu ison is clicked.
+       */
+      var menu = document.querySelector('#menu');
+      var main = document.querySelector('main');
+      var filterOp = document.querySelector('.filter-options');
+
+      menu.addEventListener('click', function(e) {
+        filterOp.classList.toggle('open');
+        e.stopPropagation();
+      });
+      main.addEventListener('click', function() {
+        filterOp.classList.remove('open');
+      });
