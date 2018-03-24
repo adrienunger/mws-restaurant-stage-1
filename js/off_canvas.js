f1bbe3b7e369221cb8db64a59bb9c7d49b22cad3
Adrien <email@email.com>
2018-03-24 20:06:41 +0100

    feat: made filter-options window for small screen sizes with off-canvas pattern accessible
    * set appropriate aria-roles and states
    * made the toggle button a button instead of a link
    * javascript onclick events control aria-states
    * remove filter-options from tab-order when minimized(off-canvas)
    * minor style + layout changes

--- js/off_canvas.js
+++ js/off_canvas.js
@@ -6,9 +6,17 @@
       var filterOp = document.querySelector('.filter-options');
 
       menu.addEventListener('click', function(e) {
+        //set aria-expanded state
+        if(menu.getAttribute("aria-expanded")=="true"){
+          menu.setAttribute("aria-expanded", "false");
+        }else{
+          menu.setAttribute("aria-expanded", "true");
+        }
+
         filterOp.classList.toggle('open');
         e.stopPropagation();
       });
       main.addEventListener('click', function() {
+        menu.setAttribute("aria-expanded", "false");
         filterOp.classList.remove('open');
       });
